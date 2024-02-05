460a29bb61ce9d49125a26768348910177e9c1c0
NeptuneXralykS <158981277+NeptuneXralykS@users.noreply.github.com>
2024-02-04 23:57:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import { Buffer } from 'buffer';
-window.Buffer = Buffer;
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // Adjust this import if necessary
+import reportWebVitals from './reportWebVitals'; // If you use this
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,14 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Optionally, call reportWebVitals here or other code
